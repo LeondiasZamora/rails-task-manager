9a0872cdf343b0ce250cb3c670eed8e6c581071d
Leonidas Zamora Gottwald <leonidas.zamora.gottwald@gmail.com>
2024-11-12 13:02:08 +0100

    finished the refatoring. now time for lunch break

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:edit, :update, :show, :destroy]
   def index
     @tasks = Task.all
   end
@@ -13,30 +14,29 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     redirect_to tasks_path
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
 
     redirect_to task_path(@task)
   end
 
   def show
-    @task = Task.find(params[:id])
     @task.save
   end
 
   private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
